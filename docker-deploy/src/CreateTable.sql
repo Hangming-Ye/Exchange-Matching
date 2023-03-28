d84db8e99be259133da00e5983582cb0e6efbd7a
ht175 <ht175@duke.edu>
2023-03-28 14:50:45 -0400

    sql table v1

---
+++ docker-deploy/src/CreateTable.sql
@@ -0,0 +1,40 @@
+CREATE TABLE ACCOUNT(
+    accout_id SERIAL,
+    balance REAL,
+    PRIMARY KEY (accout_id)
+);
+
+CREATE TYPE StatusType AS ENUM('open','canceled','executed');
+CREATE TABLE ORDER(
+    order_id SERIAL,
+    symbol VARCHAR(256),
+    remain_amount REAL,
+    limit_price REAL,
+    status StatusType,
+    accout_id INT,
+    time BIGINT,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (accout_id) REFERENCES ACCOUNT(accout_id) ON DELETE SET NULL ON UPDATE CASCADE,
+);
+
+CREATE TABLE EXECUTED( 
+    executed_id SERIAL,
+    order_id INT,
+    price REAL,
+    time BIGINT,
+    PRIMARY KEY (executed_id),
+    FOREIGN KEY (order_id) REFERENCES ORDER(order_id) ON DELETE SET NULL ON UPDATE CASCADE,
+);
+
+CREATE TABLE POSITION( 
+    position_id SERIAL,
+    accout_id, INT
+    amount real,
+    symbol VARCHAR(256),
+    PRIMARY KEY (position_id),
+    FOREIGN KEY (accout_id) REFERENCES ACCOUNT(accout_id) ON DELETE SET NULL ON UPDATE CASCADE,
+);
+
+
+
+
