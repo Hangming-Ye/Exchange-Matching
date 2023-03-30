fc51377a3d11cba8ee3c48cf674ad3cf93fbbc80
Hangming-Ye <Hangming_Ye@outlook.com>
2023-03-29 21:23:02 -0400

    implement create order

--- docker-deploy/src/orderUtils.py
+++ docker-deploy/src/orderUtils.py
@@ -1,22 +1,94 @@
 from orm import *
 from db import *
 from sqlalchemy.orm import query, Session, sessionmaker
+import time
+# 自己创建的order自己应该不能搜索到吧？
 
-def matchOrder(session, od):
+def createOrder(session, amount, price, sym, uid):
+    user = session.query().get(uid)
+    if user == None:
+        return -1
+    user_dict = user.dto()
+
+    # buy order
+    if amount > 0:
+        user_dict['balance'] -= amount * price
+        # insufficient balance
+        if user_dict['balance'] < 0:
+            return -2
+        user.update(user_dict, synchronize_session = False)
+    #sell order
+    elif amount < 0:
+        stock = session.query(Position).filter(Position.account_id == uid, Position.symbol == sym).first()
+
+        # position not find
+        if stock == None:
+            return -3
+        
+        stock = stock[0]
+        stock_dict = stock.dto()
+        stock_dict['amount'] -= amount
+
+        # position amount insufficient
+        if stock_dict['amount'] < 0:
+            return -4
+        stock.update(stock_dict, synchronize_session = False)
+
+    # create order successfully
+    order = Order(remain_amount = amount, limit_price = price, symbol = sym, time = int(time.time()), account_id = uid)
+    session.add(order)
+    session.commit()
+    return order.tran_id
+
+def matchOrder(session, od_id):
+    od = session.query(Order).get(od_id)
     allOrder = session.query(Order.tran_id).filter(Order.symbol==od.symbol, Order.status=="open")
+
     # buy order
     if od.remain_amount > 0:
-        ans = allOrder.filter(Order.limit_price < od.limit_price, Order.remain_amount < 0).order_by(Order.limit_price, Order.time).first()
+        ans = allOrder.filter(Order.limit_price <= od.limit_price, Order.remain_amount < 0).order_by(Order.limit_price, Order.time).first()
+    # sell order
     elif od.remain_amount < 0:
-        ans = allOrder.filter(Order.limit_price > od.limit_price, Order.remain_amount > 0).order_by(Order.limit_price.desc(), Order.time).first()
-    return ans
+        ans = allOrder.filter(Order.limit_price >= od.limit_price, Order.remain_amount > 0).order_by(Order.limit_price.desc(), Order.time).first()
+
+    # matched order find
+    if ans != None:
+        return ans[0]
+    # no matched order find
+    else:
+        return -1
+
+def executeOrder(session, od1_id, od2_id):
+    od1 = session.query(Order).get(od1_id)
+    od2 = session.query(Order).get(od2_id)
+
+    price  = od1.limit_price
+    
+    if od1.remain_amount > 0:
+        buy = od1
+        sell = od2
+    else:
+        buy = od2
+        sell = od1
+    
+    
+    
+
 
 def test():
-    
     engine = initDB()
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
-    tmp  = Order(symbol = "BTC", remain_amount = 10, limit_price = 100.0, status = "open", time = 111, account_id = 0)
+    testaccount = Account(balance = 114514)
+    test = Order(symbol = "BTC", remain_amount = -10, limit_price = 90.0, status = "open", time = 200, account_id = 1)
+    session.add(testaccount)
+    session.commit()
+    acc = session.query(Account.account_id).all()
+    for a in acc:
+        print(a[0])
+    session.add(test)
+    session.commit()
+    tmp  = Order(symbol = "BTC", remain_amount = 10, limit_price = 100.0, status = "open", time = 111, account_id = 2)
     ans = matchOrder(session, tmp)
     print(ans)
 test()
--- docker-deploy/src/orm.py
+++ docker-deploy/src/orm.py
@@ -14,6 +14,12 @@ class Account(Base):
     account_id = Column(Integer, Sequence('account_id_seq'), primary_key = True)
     balance = Column(REAL)
 
+    def dto(self):
+        ans = dict()
+        ans['account_id'] = self.account_id
+        ans['balance'] = self.balance
+        return ans
+
 class Order(Base):
     __tablename__ = 'order'
     tran_id = Column(Integer, Sequence('tran_id_seq'), primary_key = True)
@@ -23,13 +29,35 @@ class Order(Base):
     status = Column(Enum(StatusEnum))
     time = Column(BIGINT)
     account_id = Column(Integer, ForeignKey('account.account_id',ondelete="SET NULL", onupdate="CASCADE"))
+    
+    def dto(self):
+        ans = dict()
+        ans['tran_id'] = self.tran_id
+        ans['symbol'] = self.symbol
+        ans['remain_amount'] = self.remain_amount
+        ans['limit_price'] = self.limit_price
+        ans['status'] = self.status
+        ans['time'] = self.time
+        ans['account_id'] = self.account_id
+        return ans
+
 
 class Executed(Base):
     __tablename__ = 'executed'
     executed_id = Column(Integer, Sequence('executed_id_seq'), primary_key = True)
     order_id = Column(Integer, ForeignKey('order.tran_id',ondelete="SET NULL", onupdate="CASCADE"))
     price = Column(REAL)
+    amount = Column(REAL)
     time = Column(BIGINT)
+
+    def dto(self):
+        ans = dict()
+        ans['executed_id'] = self.executed_id
+        ans['order_id'] = self.order_id
+        ans['price'] = self.price
+        ans['amount'] = self.amount
+        ans['time'] = self.time
+        return ans
 
 class Position(Base):
     __tablename__ = 'position'
@@ -37,3 +65,11 @@ class Position(Base):
     account_id = Column(Integer, ForeignKey('account.account_id',ondelete="SET NULL", onupdate="CASCADE"))
     amount = Column(REAL)
     symbol = Column(String(256))
+
+    def dto(self):
+        ans = dict()
+        ans['position_id'] = self.position_id
+        ans['account_id'] = self.account_id
+        ans['amount'] = self.amount
+        ans['symbol'] = self.symbol
+        return ans
